aafd24181f83446a33bb0dbb655a681afc901670
dasuchka <70776259+dasuchka@users.noreply.github.com>
2020-10-21 23:57:33 +0300

    feat: have written  the code for reverse-int

---
+++ src/index.js
@@ -0,0 +1,8 @@
+module.exports = function reverse(number){
+    let str_number=String(number);
+    let reverse_number='';
+    for (let i=str_number.length-1; i>=0; i--){
+        reverse_number=reverse_number+str_number[i];
+    }
+    return Number(reverse_number);
+}
