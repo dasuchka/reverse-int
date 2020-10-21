5f66b246e371481f9f6ff4a51f253cce32303411
dasuchka <70776259+dasuchka@users.noreply.github.com>
2020-10-22 00:12:11 +0300

    refactor: correct mistake

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 module.exports = function reverse(number){
     let str_number=String(number);
+    if (number<0){
+        str_number=String(-number);
+    }
     let reverse_number='';
     for (let i=str_number.length-1; i>=0; i--){
         reverse_number=reverse_number+str_number[i];
